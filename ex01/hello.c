d3140f4e564b04d221f02cb2b80b00a7d74cce07
lixin-cpu <3094898883@qq.com>
2026-04-30 21:49:23 +0800

    增强功能

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,15 @@
-#include "stdio.h"
+#include <stdio.h>
+
+int input() {
+    int n;
+    scanf("%d", &n);
+    return n;
+}
 
 int main() {
     printf("Hello, World!\n");
+    printf("请输入：");
+    int num = input();  
+    printf("你输入了：%d\n", num);
     return 0;
 }
